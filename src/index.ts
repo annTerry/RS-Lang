a29b4cba717e57fc38c7665abbdd82eb35c519a5
annTerry <annak@1gb.ru>
2022-08-18 00:48:53 +0300

    feat: add get api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import './style.scss';
+import GetData from './api/getData';
 
+const fetchData = new GetData();
+fetchData.getData('words?group number=0&page=0', (data) => {
+  data.forEach((el) => console.log(el));
+});
 console.log('Works!');
